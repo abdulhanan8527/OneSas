bf46e80f20b59f8b801e8753f281d99b34db1c1b
abdulhanan8527 <honeykhan8527@gmail.com>
2025-04-04 00:19:06 +0500

    Vercel YT

--- api/index.py
+++
@@ -1,5 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OneSas.settings')
-app = get_wsgi_application()  # Vercel requires 'app' variable
