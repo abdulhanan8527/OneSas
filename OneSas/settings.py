07e6e41ef2f8f9411d9423e8eb7d22b743554f02
abdulhanan8527 <honeykhan8527@gmail.com>
2025-04-04 02:09:08 +0500

    done

--- OneSas/settings.py
+++ OneSas/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o9x(70&_x4-vwv#qri&)iqk7s_2su9%yqqj%a#t7nnz22^(k5h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'www.1sas.co', '1sas.co', '127.0.0.1']
 
