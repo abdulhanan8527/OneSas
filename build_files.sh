cd243f40a8a1af507f1db28eb2b4677479156798
abdulhanan8527 <honeykhan8527@gmail.com>
2025-04-04 02:56:51 +0500

    maybe

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,2 @@
-# #!/bin/bash
-# # Create staticfiles directory if it doesn't exist
-# mkdir -p staticfiles
-# # Copy static files from OneSas_app to staticfiles
-# cp -r OneSas_app/static/* staticfiles/
-# # Run collectstatic
-# python manage.py collectstatic --noinput
+pip install -r requirements.txt
+python manage.py collectstatic 
